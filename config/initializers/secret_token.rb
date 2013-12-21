6dffae3b482572ef60538bba206ffd2d11142ed9
steve <stevedanko1@gmail.com>
2013-12-21 14:45:46 -0500

    set up development enviorment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DogNetwork::Application.config.secret_key_base = 'a7eec26e08453fe778d4c563645f74676670ff2ddbc3feb59a6251d626fdb8a42be8be4b1b442ae8acf2acc9d2be9a9b6ec49a6282c27d1d8bd6a970b55b2377'
